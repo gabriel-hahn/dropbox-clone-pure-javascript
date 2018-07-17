f7aafd7633f85b0e6abcf799c9cc282e5c24d2ac
Gabriel Hahn Schaeffer <gabriel_hahn@hotmail.com>
2018-07-17 20:40:42 -0300

    UploadTask method
    UplodaTask methot to send files to server.

--- app/public/src/controller/DropBoxController.js
+++ app/public/src/controller/DropBoxController.js
@@ -13,7 +13,40 @@ class DropBoxController {
         });
 
         this.inputFilesEl.addEventListener('change', event => {
+            this.uploadTask(event.target.files);
             this.snackModalEl.style.display = 'block';
         });
+    }
+
+    uploadTask(files) {
+        let promises = [];
+
+        //For each file that will upload, we've a promise
+        [...files].forEach(file => {
+            promises.push(new Promise((resolve, reject) => {
+                let ajax = new XMLHttpRequest();
+
+                ajax.open('POST', '/upload');
+
+                ajax.onload = event => {
+                    try {
+                        resolve(JSON.parse(ajax.responseText));
+                    } catch (e) {
+                        reject(e);
+                    }
+                }
+
+                ajax.onerror = event => {
+                    reject(event);
+                }
+
+                let formData = new FormData();
+                formData.append('input-file', file);
+
+                ajax.send(formData);
+            }));
+        });
+
+        return Promise.all(promises);
     }
 }
