1767b8c694618382853f4cd77a6dcf1623573c98
Gabriel Hahn Schaeffer <gabriel_hahn@hotmail.com>
2018-07-21 17:29:40 -0300

    Open files
    Open files with fs

--- app/routes/index.js
+++ app/routes/index.js
@@ -8,6 +8,28 @@ router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/file', (req, res) => {
+  let path = './' + req.query.path;
+  if (fs.existsSync(path)) {
+    fs.readFile(path, (err, data) => {
+      if (err) {
+        console.error(err);
+        res.status(400).json({
+          error: err
+        });
+      }
+      else {
+        res.status(200).end(data);
+      }
+    });
+  }
+  else {
+    res.status(404).json({
+      error: 'File not found.'
+    });
+  }
+});
+
 router.delete('/file', (req, res) => {
   let form = new formidable.IncomingForm({
     uploadDir: './upload',
@@ -26,7 +48,11 @@ router.delete('/file', (req, res) => {
         }
       });
     }
-    res.json({ fields });
+    else {
+      res.status(404).json({
+        error: 'File not found.'
+      });
+    }
   });
 });
 
